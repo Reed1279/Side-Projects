be75684dff511d60a48b3544c23a15be7a1a4076
Tyler Reed <30813808+Reed1279@users.noreply.github.com>
2017-12-05 18:27:02 -0500

    Update Program.cs

--- MagicLittle8-Ball/Program.cs
+++ MagicLittle8-Ball/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Speech.Synthesis;
 
 namespace Magic8Ball
 {
@@ -15,6 +16,9 @@ namespace Magic8Ball
 
 		static void Main(string[] args)
 		{
+			SpeechSynthesizer synth = new SpeechSynthesizer();
+			synth.Speak("Hello, Welcome to the Magic 8 Ball.  I am a cybernetich life form that will answer your questions with the most probable answer through simple data analysis conducted over the internet.  Yes, Google");
+
 			// Preserve current console text color
 			ConsoleColor oldColor = Console.ForegroundColor;
 
@@ -26,19 +30,25 @@ namespace Magic8Ball
 			Random randomObject = new Random();
 
 
+
 			while (true)
 			{
 				Console.ForegroundColor = ConsoleColor.Magenta;
 				Console.Write("Ask a question?: ");
+				synth.Speak("What is your question?");
 				Console.ForegroundColor = ConsoleColor.Gray;
 				string questionString = Console.ReadLine();
 
+				
+				
 				// Make the computer pause to think about the question 
 				// before answering
 				int numberOfSecondsToSleep = randomObject.Next(5) + 1;
-				Console.WriteLine("Thinking about your answer, stand by....");
-				Thread.Sleep(numberOfSecondsToSleep * 1000);
-
+				Console.WriteLine("Thinking about your answer, please stand by....");
+				synth.Speak("Computing... ");
+				Thread.Sleep(numberOfSecondsToSleep * 600);
+			
+				
 
 				if(questionString.Length == 0)
 				{
@@ -53,9 +63,10 @@ namespace Magic8Ball
 				}
 
 				// If the user insults the Magic 8 Ball
-				if(questionString.ToLower() == "you suck")
+				if (questionString.ToLower() == "you suck")
 				{
 					Console.WriteLine("You suck even more! Bye!!");
+					synth.Speak("I am deeply offended by your tone. Goodbye!");
 					break;
 				}
 
@@ -67,41 +78,49 @@ namespace Magic8Ball
 					case 0:
 						{
 							Console.WriteLine("Oh you better believe it!");
+							synth.Speak("Oh, you better believe it!");
 							break;
 						}
 					case 1:
 						{
 							Console.WriteLine("I like to think I know something.");
+							synth.Speak("I am a intelegent piece of hardware, so I like to think I know something.");
 							break;
 						}
 					case 2:
 						{
 							Console.WriteLine("HAHAH OK!");
+							synth.Speak("Ha Ha Ha, Ok!");
 							break;
 						}
 					case 3:
 						{
 							Console.WriteLine("YES!");
+							synth.Speak("Yes, I like to think so.");
 							break;
 						}
 					case 4:
 						{
 							Console.WriteLine("NO!");
+							synth.Speak("No!");
 							break;
 						}
 					case 5:
 						{
 							Console.WriteLine("Well, it's worth a shot.");
+							synth.Speak("Well, it was worth a shot.");
 							break;
 						}
 					case 6:
 						{
 							Console.WriteLine("I dont know if that is a good idea!");
+							synth.Speak("I don't know if it's a good idea to answer that.");
 							break;
 						}
 					case 7:
 						{
 							Console.WriteLine("Fat chance in hell!");
+							synth.Speak("Ha Ha Ha, Fat chance in hell.");
 							break;
 						}
 				}
@@ -111,5 +130,6 @@ namespace Magic8Ball
 		}
 	}
 }
+
 
 
